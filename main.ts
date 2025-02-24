1645b66f7c0e0cfdfcf76b1d9ae31aed4ae3b77b
MarekDvorak09 <marek.dvorak.024@pslib.cz>
2025-02-24 09:54:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,44 @@
-radio.setGroup(205)
-music.setVolume(100)
+radio.setGroup(158)
+music.setVolume(200)
+let lock: boolean = false
 let start: number
 let difference:number
-Sensors.SetLightLevel()
+
 radio.onReceivedNumber(function (receivedNumber: number){
     basic.showNumber(receivedNumber)
-    difference = receivedNumber - input.runningTime()
     console.log(receivedNumber)
 })
 radio.onReceivedValue(function (name: string, receivedNumber2: number) {
-    if (name = "time")
-        start = difference - receivedNumber2
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(runTime/1000)
+    if (name = "start")
+        start = input.runningTime()
+        lock = false
 })
 
 let finish: number
+Sensors.SetLightLevel()
+
+
 Sensors.OnLightDrop(function() {
+   if (!lock){
+    lock = true
     finish = input.runningTime()
+    music.playTone(Note.A,500)
+    radio.sendNumber(finish - start)
+    basic.showNumber((finish - start)/1000)
+   }
 })
-let runTime = finish - start
 
-
+input.onButtonPressed(Button.A, function () {
+    while (lock === true){
+    music.playTone(Note.D, 250)
+    radio.sendNumber(finish - start)
+    basic.showNumber((finish - start)/1000)
+    lock = false
+    }
+    lock = true
+})
+radio.onReceivedString(function (name: string) {
+    if (name = "unlock")
+    lock = false
+    music.playTone(Note.D3, 400)
+})
